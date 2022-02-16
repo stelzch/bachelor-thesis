8ea0eda1a6181356deda8772a71218f20afb55bd
Christoph Stelz <christoph.stelz@student.kit.edu>
2022-02-16 09:27:50 +0100

    Add violin plots to script

--- scripts/benchmark_plot.py
+++ scripts/benchmark_plot.py
@@ -3,22 +3,49 @@
 
 # In[2]:
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 import sqlite3
 from matplotlib.ticker import EngFormatter
+from statistics import median
 
 def fetch_data(run_id, mode):
     cur.execute(f"SELECT n_summands, datafile, time_ns, stddev, repetitions FROM results WHERE mode = ?" \
                 "AND run_id = ? ORDER BY n_summands",
                (mode, run_id))
-
 def xye(data):
     x = list(map(lambda x: x[0], data)) # n_summands
     y = list(map(lambda x: x[2] / 1e9, data)) # time_ns
     e = list(map(lambda x: x[3] / 1e9, data)) # stddev
     return x, y, e
+
+def fetch_durations(run_id, datasetQuery='%'):
+    plot_data = [[]]
+    runs = cur.execute(f"SELECT id, mode, n_summands, datafile, ranks FROM results" \
+                    " WHERE run_id = ? AND datafile LIKE ? ORDER BY n_summands, datafile, mode", (run_id,datasetQuery))
+    prev_datafile = None
+    
+    for result_id, mode, n_summands, datafile, ranks in runs.fetchall():
+        if mode == "baseline":
+            continue
+
+        if prev_datafile is not None and prev_datafile != datafile:
+            plot_data.append([])
+        cur.execute("SELECT time_ns / 1e9 FROM durations WHERE result_id = ?", (result_id,))
+        
+        durations = list(map(lambda x: x[0], cur.fetchall()))
+        plot_data[-1].append({
+            "mode": mode,
+            "datafile": datafile,
+            "n": n_summands,
+            "m": ranks,
+            "durations": durations
+        })
+        
+        
+        prev_datafile = datafile
+    return plot_data
+
 
 db = 'file:project/benchmarks/results.db?mode=ro'
 con = sqlite3.connect(db, uri=True)
@@ -76,6 +103,55 @@ def scatter_plot(run_id):
 
     return f
 
+def violin_plot(run_id, datasetQuery):
+    plot_data = fetch_durations(run_id, datasetQuery)
+    rows = len(plot_data)
+    cols = max(map(lambda x: len(x), plot_data))
+    formatter0 = EngFormatter(unit='s', places=2)
+
+    f, axs = plt.subplots(nrows=rows, figsize=(7, 4 * rows))
+
+    i = 0
+
+    colorpalette = ["#034670ff", "#2f779dff", "#887006ff", "#ac360bff"]
+
+    for dataset in plot_data:
+        ax = axs[i] if rows > 1 else axs
+
+        mode_durations = list(map(lambda x: x["durations"], dataset))
+
+        ax.set_ylabel("Time")
+        ax.yaxis.set_major_formatter(formatter0)
+        ax.xaxis.can_zoom = False
+
+        ax.get_xaxis().set_visible(True)
+
+        # Ticks for each mode
+        ax.get_xaxis().set_ticks(range(1, len(dataset)+1))
+
+        medians = list(map(lambda d: median(d), mode_durations))
+        labels = map(lambda x: x["mode"] + f" ({formatter0.format_eng(median(x['durations']))}s)", dataset)
+        ax.get_xaxis().set_ticklabels(labels)
+
+
+        #ax.yaxis.set_ticks(y_ticks)
+        #ax.yaxis.set_ticks_position('both')
+        #plt.setp(ax.get_yticklabels(), rotation=30, horizontalalignment='right')
+
+        n = format(dataset[0]["n"], ",d").replace(",", " ")
+        m = dataset[0]["m"]
+        filename = dataset[0]["datafile"][5:]
+        ax.set_title(f"{filename}, N={n}, p={m}")
+
+        violins = ax.violinplot(mode_durations, showmedians=True, widths=0.5)
+        for body, color in zip(violins["bodies"], colorpalette):
+            body.set_color(color)
+
+        i += 1
+    margin_y = 0.08 / rows
+    plt.subplots_adjust(left=0.15, bottom=0.0 + margin_y, right=0.98, top=1.0 - margin_y)
+    return f
+
 def last_complete_benchmark(p=256):
     cur.execute("""
         SELECT run.id, COUNT(result.id) FROM runs run, results result
@@ -90,6 +166,9 @@ def last_complete_benchmark(p=256):
     
     
 scatter_plot(last_complete_benchmark()).savefig("figures/benchmarkScatter.pdf")
+violin_plot(last_complete_benchmark(), '%354%').savefig("figures/violin354.pdf")
+violin_plot(last_complete_benchmark(), '%rokasD4%').savefig("figures/violinRokasD4.pdf")
+violin_plot(last_complete_benchmark(), '%rokasD7%').savefig("figures/violinRokasD7.pdf")
 
 # In[ ]:
 
